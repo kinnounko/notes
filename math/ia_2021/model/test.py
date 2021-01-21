f93185a1dbc0c17b1721cc9d677e96d6381861b8
kinnounko <kinnounko7@gmail.com>
2021-01-21 01:40:28 +0100

    worked on rp

--- math/ia_2021/model/test.py
+++ math/ia_2021/model/test.py
@@ -1,7 +1,9 @@
 import matplotlib.pyplot as plt
+import numpy as np
 import pandas as pd
 
-df = pd.read_table("test.csv", sep='\t')
+df = pd.read_csv("test.csv")
 
+plt.plot(np.unique(df['aaa']), np.poly1d(np.polyfit(df['aaa'], df['test'], 1))(np.unique(df['aaa'])))
 plt.scatter(df['aaa'], df['test'], marker="x")
 plt.show()
